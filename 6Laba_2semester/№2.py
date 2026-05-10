05b85ceca117302cb84dd926fa26714aa728d24a
Данил Торов <k0f3st72@MacBook-Air-Danil.local>
2026-05-10 15:53:07 +0300

    изменил chat.html

--- 6Laba_2semester/№2.py
+++ 6Laba_2semester/№2.py
@@ -10,12 +10,3 @@ print("Среднее значение: ", np.mean(a))
 print("Минимальный элемент :", np.min(a))
 print("Максимальный элемент :", np.max(a))
 print("Отклонение: ", np.std(a))
-
-# ============Python==================
-py_list = [15, 23, 32, 52, 46, 75]
-py_list2 = [1, 2, 3, 4, 5, 6,7]
-
-print("Сумма: ", sum(py_list))
-print(f"Среднее: ", sum(py_list)/len(py_list))
-print(f"Минимум: ", min(py_list))
-print(f"Максимум: ", max(py_list))
